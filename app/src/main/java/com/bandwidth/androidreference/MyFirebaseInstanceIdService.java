38e6021b904021287bd49d9f01dccb914dc21766
Isac Souza <isouza@daitangroup.com>
2016-07-15 14:17:06 -0300

    Send push registration to server

--- app/src/main/java/com/bandwidth/androidreference/MyFirebaseInstanceIdService.java
+++ app/src/main/java/com/bandwidth/androidreference/MyFirebaseInstanceIdService.java
@@ -1,12 +1,18 @@
 package com.bandwidth.androidreference;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
+import cz.acrobits.libsoftphone.Instance;
+
 public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
-    private static final String TAG = "MyFirebaseInstanceIdSer";
+    private static final String TAG = MyFirebaseInstanceIdService.class.getSimpleName();
+
+    private final Handler mainThreadHandler = new Handler(Looper.getMainLooper());
 
     @Override
     public void onTokenRefresh() {
@@ -17,7 +23,12 @@ public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
         sendRegistrationToServer(refreshedToken);
     }
 
-    private void sendRegistrationToServer(String refreshedToken) {
-        
+    public void sendRegistrationToServer(final String token) {
+        mainThreadHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                Instance.Notifications.Push.setRegistrationId(token);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/bandwidth/androidreference/MyFirebaseMessagingService.java
@@ -0,0 +1,17 @@
+package com.bandwidth.androidreference;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = MyFirebaseMessagingService.class.getSimpleName();
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        Log.i(TAG, "Received new message: " + remoteMessage.getData());
+    }
+}
